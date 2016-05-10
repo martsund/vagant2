e0b8e55c56150e286859b17f69cef9a417907bd2
Martin Sund <martinsund@outlook.dk>
2016-05-10 11:10:17 +0200

    adding files for LAMP stack

---
+++ scripts/centos-lamp.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Update CentOS with any patches 
+yum update -y --excluede=kernel
+
+# Tools
+yum install -y nano git unzip screen
+
+# Apache
+yum install -y httpd httpd-devel httld-tools
+chkconfig --add httpd
+chkconfig httpd on
+service httpd stop
+
+rm -rf /var/www/html
+ln -s /vagrant /var/www/html
+
+service httpd start
+
+# PHP
+yum install -y php php-cli php-common php-devel php-mysql
+
+"MySQL
+yum install -y mysql mysql-server mysql-devel
+chconfig --add mysqld
+chconfig mysqld on
+
+service mysqld start
+
+mysql -u root -e "SHOW DATABASES";
+
+# Download Start Content
+
+service httpd restart 
