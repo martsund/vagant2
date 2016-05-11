fdcf0f75b7c13bfeba318fa95c5145c430dd68a7
Martin Sund <martinsund@outlook.dk>
2016-05-11 21:03:52 +0200

    adding and upgraded html file and exam file

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# install Git and Nano
-yum install -y git
-yum install -y nano
+echo "Installing Apache and setting it up..."
+apt-get update >/dev/null 2>&1
+apt-get install -y apache2 >/dev/null 2>&1
+rm -rf /var/www
+ln -fs /vagrant /var/www
