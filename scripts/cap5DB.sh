b0ba7cabea19397d282f9c99cf05924c88e3c2e0
Martin Sund <martinsund@outlook.dk>
2016-05-11 16:27:04 +0200

    adding and upgraded html file and capDB5 file

--- scripts/cap5DB.sh
+++ scripts/cap5DB.sh
@@ -1,7 +1,9 @@
-export DEBIAN_FRONTEND=noninteractive
-apt-get update
-apt-get install -y mysql-server
-sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mysql/my.cnf
-restart mysql
-mysql -uroot mysql <<< "GRANT ALL ON *.* TO 'root'@'%'; FLUSH PRIVILEGES;"
-service mysql start
+#!/usr/bin/env bash
+
+DB=$1;
+#clean up first
+echo "Droping database $DB if it already exists.";
+mysql -uhomestead -psecret -e "DROP DATABASE IF EXISTS $DB";
+
+echo "Creating new database $DB";
+mysql -uhomestead -psecret -e "create database $DB";
