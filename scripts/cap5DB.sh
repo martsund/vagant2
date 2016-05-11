9cda5b3c99a4bac21847b2387bb2374e0b814627
Martin Sund <martinsund@outlook.dk>
2016-05-11 17:01:30 +0200

    adding and upgraded html file and capDB5 file

--- scripts/cap5DB.sh
+++ scripts/cap5DB.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
-
 DB=$1;
+DUMPFILE=$2;
+
 #clean up first
 echo "Droping database $DB if it already exists.";
 mysql -uhomestead -psecret -e "DROP DATABASE IF EXISTS $DB";
 
 echo "Creating new database $DB";
 mysql -uhomestead -psecret -e "create database $DB";
+
+echo "Importing data to $DB from $DUMPFILE";
+mysql -u martsund -p secret -h localhost -e "$DB < $DUMPFILE";
