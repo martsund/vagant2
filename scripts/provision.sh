61b0fbe50f6b7f16179ffd59c7a7af2295820e9a
Martin Sund <martinsund@outlook.dk>
2016-05-11 22:15:51 +0200

    adding and upgraded new provision file for web

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -5,3 +5,13 @@ apt-get update >/dev/null 2>&1
 apt-get install -y apache2 >/dev/null 2>&1
 rm -rf /var/www
 ln -fs /vagrant /var/www
+
+
+# Install PHP
+apt-get -y install php5 apache2 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt php5-mysql php-apc > /dev/null 2>&1
+
+# Download Starter Content
+cd /vagrant
+sudo -u vagrant wget -q https://raw.githubusercontent.com/martsund/vagant2/master/files/index.html
+sudo -u vagrant wget -q https://raw.githubusercontent.com/martsund/vagant2/master/files/info.php
+ 
