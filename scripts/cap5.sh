5c481a3d53ac464856a438a4f7bcbc6dfea97d4c
Martin Sund <martinsund@outlook.dk>
2016-05-11 18:15:43 +0200

    adding and upgraded html file and cap5 and DB5 file

--- scripts/cap5.sh
+++ scripts/cap5.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# Update CentOS with any patches 
-yum update -y --exclude=kernel
-
-# Tools
-yum install -y nano git unzip screen
-
-# Apache
-yum install -y httpd httpd-devel httpd-tools
-chkconfig --add httpd
-chkconfig httpd on
-service httpd stop
-
 rm -rf /var/www/html
 ln -s /vagrant /var/www/html
 
--- scripts/cap5DB.sh
+++ scripts/cap5DB.sh
@@ -1,13 +1,11 @@
-#!/usr/bin/env bash
-DB=$1;
-DUMPFILE=$2;
+#!/bin/bash
 
-#clean up first
-echo "Droping database $DB if it already exists.";
-mysql -uhomestead -psecret -e "DROP DATABASE IF EXISTS $DB";
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get install -y mysql-server
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mysql/my.cnf
+restart mysql
+mysql -uroot msql <<< "GRANT ALL ON *.* TO 'root'@'%'; FLUSH PRIVILEGES;"
 
-echo "Creating new database $DB";
-mysql -uhomestead -psecret -e "create database $DB";
 
-echo "Importing data to $DB from $DUMPFILE";
-mysql -u martsund -p secret -h localhost -e "$DB < $DUMPFILE";
+
